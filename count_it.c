edaa965a933ced5a5a43be15493baec92586cefd
DTBissy <isaactheedwards@gmail.com>
2024-01-20 02:40:42 +0000

    PO

---
+++ count_it.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	
+	while( a < 20What are the purpose of the gcc flags -m32 and -m64 ) {
+		printf("Valuse of a: %d\n", a);
+		a++;
+	}
+
+	return (0);
+}
