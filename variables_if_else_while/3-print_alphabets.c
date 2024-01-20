a59fc2270d50d571503a1b292af0b9ae467ea038
DTBissy <isaactheedwards@gmail.com>
2024-01-20 03:54:36 +0000

    PO

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Printing out the alphabet
+* Return: 0
+*/
+int main(void)
+{
+        char c;
+
+	char b;
+
+        c = 'a';
+
+	b = 'A';
+
+        while (c <= 'z')
+        {
+                putchar(c);
+                c++;
+        }
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+        putchar('\n');
+
+        return (0);
+}
