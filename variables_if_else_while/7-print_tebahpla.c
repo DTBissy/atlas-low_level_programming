d794fa895bb4069b2468219dba41e3ff91514365
DTBissy <isaactheedwards@gmail.com>
2024-01-21 04:44:22 +0000

    PO

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Print lower case in reverse
+* Return: 0
+*/
+
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while
+		(a >= 'a') {
+			putchar(a);
+			a--;
+		}
+	putchar('\n');
+	return (0);
+}
