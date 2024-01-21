ae7c5b7c1fe7586fb6ea0f6fa21a2092c13567fa
DTBissy <isaactheedwards@gmail.com>
2024-01-21 05:53:49 +0000

    po

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Print all combinations of a single digit number
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
