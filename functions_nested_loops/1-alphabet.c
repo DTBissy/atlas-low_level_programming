189a3925d53df1b7a1b49b8cc18332ce92657f80
DTBissy <isaactheedwards@gmail.com>
2024-01-22 16:58:20 +0000

    PO

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - Prints the alpha bet with prototype
 * Return: 0
 */
 
-void print_alphabet(void)	
+void print_alphabet(void)
 {
-	int a;
+	char a;
 
-
-
-int main()
-{
-	print_alphabet();
-	return (0);
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n');
 }
