3f9bbfdddbbedbba07a144c03097105ad04480bd
DTBissy <isaactheedwards@gmail.com>
2024-01-22 22:46:49 +0000

    PO

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - function that sums two numbers
+* @a parameter 2
+* @b parameter 2
+* return: 0
+*/
+
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+
+
