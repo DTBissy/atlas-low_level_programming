0b7722fa425342d7f52321c06d8a3755f8465b2c
DTBissy <isaactheedwards@gmail.com>
2024-01-24 20:54:33 +0000

    Return can be used so simply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - Multiplies to integers
+* @a - function parameter
+* @b - function parameter
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
