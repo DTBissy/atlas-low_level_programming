90aecda6d6241be04d1bec221101814fabcd6f5f
DTBissy <isaactheedwards@gmail.com>
2024-01-26 05:22:21 +0000

    Putchar needs % and / to print more than one digit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - Prints numbers but 2
+*
+* Return: void
+*
+*/
+
+
+void more_numbers(void)
+{
+	int a;
+
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+				putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
