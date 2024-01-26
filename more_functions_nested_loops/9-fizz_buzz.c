4b19d11d4729beea2043cb80ef2880350f16c0f4
DTBissy <isaactheedwards@gmail.com>
2024-01-26 23:08:57 +0000

    Fizzin and Buzzin

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - Print Fizz and Buzz
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+                        printf("%s ", "FizzBuzz");
+		else if (a % 5 == 0)
+			printf("%s ", "Buzz");
+		else if (a % 3 == 0)
+			printf("%s ", "Fizz");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
