c6a137aa8aee68da7e6234bc42cb828af15616d6
DTBissy <isaactheedwards@gmail.com>
2024-01-26 06:31:54 +0000

    Just sum underscores

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*print_line - Prints multiple lines or a new line
+*
+*Return: void
+*/
+void print_line(void)
+{
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; n++)
+			putchar('_');
+		putchar('\n')
+	}
+}
