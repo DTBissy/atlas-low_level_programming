757daf30e367175f78ecfcaec3e48aad1230ed21
DTBissy <isaactheedwards@gmail.com>
2024-01-26 22:58:52 +0000

    % modulo % modulo % modulo

---
+++ more_functions_nested_loops/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+                        printf("%s ", "FizzBuzz");
+		else if (a % 5 == 0)
+			printf("%s ", "Buzz");
+		else if (a % 3 == 0)
+			printf("%s ", "Fizz");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
