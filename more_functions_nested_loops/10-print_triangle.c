71bd5862e47bf1842d013bf22c07752540c5d369
DTBissy <isaactheedwards@gmail.com>
2024-01-27 00:45:33 +0000

    I WANT TO DIE

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_triangles - Prints a triangle
+* @size: function parameter
+* Return: 0
+*/
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		putchar('\n');
+	else
+	{
+		int a, b, c;
+
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= (size - a); b++)
+				putchar(' ');
+			{
+				for (c = 1; c <= a; c++)
+					putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
