c20b65a1c8e3cf629edc3ecf7c158d276520c2e4
DTBissy <isaactheedwards@gmail.com>
2024-01-30 05:48:46 +0000

    Reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - Reverses the string 
+* @s: function parameter
+* Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int i;
+
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char j;
+
+		j = s[i];
+	s[i] = s[count - 1 -i];
+	s[count - 1 - i] = j;
+	}
+}
