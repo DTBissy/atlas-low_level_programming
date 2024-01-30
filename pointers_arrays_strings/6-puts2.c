29341fc8502b0b1942c933389c13f0bd14aee91f
DTBissy <isaactheedwards@gmail.com>
2024-01-30 05:59:12 +0000

    PO

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <strimg.h>
+
+/**
+* puts2 - Prints every other character of string
+* @str: funtcion parameter
+* Return: 0
+*/
+
+void putca2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar (str[i]);
+	}
+	_putchar('\n');
+}
