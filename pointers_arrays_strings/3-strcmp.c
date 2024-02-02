e6d885a98ef5fd5df3f61a2491184d9bdfe3d32b
DTBissy <isaactheedwards@gmail.com>
2024-02-02 22:04:16 +0000

    Compare to nothing

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcmp - Compares two strings
+ *@s1: Function Parameter
+ *@s2: Function Parameter
+ *Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s2[i] < s1[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
