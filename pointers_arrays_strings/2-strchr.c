76eb80da634c347a3721dcd89df666f64e0c3c60
DTBissy <isaactheedwards@gmail.com>
2024-02-04 03:38:47 +0000

    STUPID STRCHR

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - Locates a string
+ * @s: Pointer Parameter
+ * @c: Function Parameter
+ * Return:0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+
+	if (c == '\0')
+	{
+		return s;
+	}
+	return NULL;
+}
