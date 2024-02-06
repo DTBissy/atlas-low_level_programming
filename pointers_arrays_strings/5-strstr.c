f4ef38ee829e362ce6265a6559c0fb10e50f5890
DTBissy <isaactheedwards@gmail.com>
2024-02-06 03:43:09 +0000

    PO

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
-
 /**
- * _strstr - Locates a substring
+ * _strstr - Locate a substrring
  * @haystack: Pointer Parameter
  * @needle: Pointer Parameter
- * Return:0
+ * Return: 0
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	return (strstr(haystack, needle));
+	while (*haystack)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*haystack && *j && *haystack == *j)
+		{
+			haystack++;
+			j++;
+		}
+		if (!*j)
+			return (i);
+		haystack = i +  1;
+	}
+	return (NULL);
 }
