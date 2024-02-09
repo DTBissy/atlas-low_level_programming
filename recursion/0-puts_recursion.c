77a1e712b886dd201939b399d2229ec56c623bf6
DTBissy <isaactheedwards@gmail.com>
2024-02-09 03:45:48 +0000

    MERRY GOLD

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: Function parameter
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}		
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}	
