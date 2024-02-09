a16959d891e340e2d19d76da306eddd10dea2c8d
DTBissy <isaactheedwards@gmail.com>
2024-02-09 04:31:14 +0000

    Nice Work SONNY

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,17 +5,17 @@
 /**
  * _strlen_recursion - Returns the length of a string
  * @s: Function Parameter
- * Return: 0 
+ * Return: 0
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
