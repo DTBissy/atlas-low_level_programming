9c857f0215ff01be5ea13250f686e3def6fce5fe
DTBissy <isaactheedwards@gmail.com>
2024-02-11 02:33:57 +0000

    PO

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,38 @@
-int is_prime(unsigned int n, unsigned int c)
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * is_prime - the checks if the number is prime
+ * @a: Pointer parameter
+ * @i: checks against a
+ * Return: "a"
+ */
+
+int is_prime(unsigned int a, unsigned int i)
 {
-	if (n % c == 0)
+	if (a % i == 0)
 	{
-		if (n == c)
+		if (a == i)
 			return (1);
 		else
 			return (0);
 	}
-	return (0 + is_prime(n, c + 1));
+	return (0 + is_prime(a, i + 1));
 }
 
 /**
  * is_prime_number - detects if an input number is a prime number
- * @n: input number
+ * @a: input number
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
 
-int is_prime_number(int n)
+int is_prime_number(int a)
 {
-	if (n == 0)
+	if (a == 0)
 		return (0);
-	if (n < 0)
+	if (a < 0)
 		return (0);
-	if (n == 1)
+	if (a == 1)
 		return (0);
-	return (is_prime(n, 2));
+	return (is_prime(a, 2));
 }
