d4147c0413a1291f99dfaacf3534269aac49a582
DTBissy <isaactheeedwars@gmail.com>
2024-02-12 05:11:07 +0000

    Eat it 7658

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,14 +9,14 @@
  */
 
 
-int main(int argc, char *argv[])
+int main(unsigned int argc, unsigned char *argv[])
 {
 	int num1;
         int num2;
         int product;
 
-        num1 = (atoi(argv[1]));
-        num2 = (atoi(argv[2]));
+        num1 = (atoi(argv[argc - 1]));
+        num2 = (atoi(argv[argc - 2]));
         product = num1 * num2;
 
 	if (argc < 3)
