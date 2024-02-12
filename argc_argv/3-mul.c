09a7b327720ee736dbc9b1f82c46cdf0674a13ab
DTBissy <isaactheeedwars@gmail.com>
2024-02-12 05:25:28 +0000

    Usgined

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,17 +8,22 @@
  * Return: 0
  */
 
-
 int main(int argc, char *argv[])
 {
+	int num1;
+        int num2;
+        int product;
+
+        num1 = (atoi(argv[argc - 1]));
+        num2 = (atoi(argv[argc - 2]));
+        product = num1 * num2;
+
 	if (argc <= 2)
 	{
 		printf("%s\n", "Error");
 		return (1);
-	}
-	else
-	{
-		printf("%d\n", (atoi(argv[argc -   1]) * atoi(argv[argc -   2])));
-		return (0);
-	}
+	}	
+	printf("%d\n", product);
+
+	return (0);
 }
