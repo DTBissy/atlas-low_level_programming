567d8c646edb0c8c4c148aaac04e4615152f64d4
DTBissy <isaactheedwards@gmail.com>
2024-02-16 01:20:54 +0000

    OK

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strdup - Moves a pointer a round while carrying th string
+ * @str: Pointer with string
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{ 
+	size_t length = strlen(str);
+
+	char *s = (char *) malloc((length + 1) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(s, str);
+	
+	return(s);
+}
