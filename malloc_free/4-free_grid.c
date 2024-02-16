c6d7f2a1e379c326d0f26e255f6be90791d266e5
DTBissy <isaactheedwards@gmail.com>
2024-02-16 05:19:57 +0000

    Easy

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - Frees my previous function
+ * @grid: My grid
+ * @height: My My
+ * Return: 0
+ */
+
+void free_grid(int **array, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(array[i]);
+	}
+	free(array);
+}
