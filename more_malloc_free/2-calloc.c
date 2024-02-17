67a09baf1b397ef37f7de5eb29ad2aae523de45f
DTBissy <isaactheedwards@gmail.com>
2024-02-17 01:55:02 +0000

    Pretty Simple

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Not sure
+ * @size: Pointer size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t bigsize = nmemb * size;
+	char *s;
+
+	if (nmemb == '\0')
+	{
+		return (NULL);
+	}
+	if (size == '\0')
+	{ 
+		return (NULL);
+	}
+	
+	s = malloc(bigsize);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	 return (s);
+}
