923609ffc65067df70a3c61b3c0843a61bd3af5b
DTBissy <isaactheedwards@gmail.com>
2024-02-17 00:37:30 +0000

    This was a long one

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include "main.h"
 
-/** string_nconcat - Concatenates two strings
+/**
+ * string_nconcat - Concatenates two strings
  * @s1: Dest pointer
  * @s2: SRC pointer
  * @n: iterator pointer
- * Retrn: 0
+ * Return: 0
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -16,23 +17,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size2 = 0;
 	unsigned int i;
 	char *s;
-
-	/** Checking function parameters pointers before we let them in */ 
-	if(s1 == NULL)
+	/** Checking function parameters pointers before we let them in */
+	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
-/** You can let function pointers create the variable while the array(size1, size2) are the Variable sizes */
+/**lets function pointers serve as variables and array *size1 are indexs */
 	while (s1[size1] != '\0')
 	{
 		size1++;
 	}
-	while(s2[size2] != '\0')
+	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
-/** like the question asked if n is greater than the length of size2 use the whole s2 */
+/**if n is greater than the length of size2 use the whole s2 */
 	if (n > size2)
 		n = size2;
 	s = malloc((size1 + n + 1) * sizeof(char));
@@ -44,11 +44,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[i] = s1[i];
 	}
-	for (;i < (size1 + n); i++)
+	for (; i < (size1 + n); i++)
 	{
 		s[i] = s2[i - size1];
 	}
 	s[i] = '\0';
 /** confused on wheter this is being free or not */
-	return(s);
+	return (s);
 }
