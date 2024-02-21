060f3eedb7a5ab37de504fd62f5998f889c69403
DTBIssy <isaactheedwards@gmail.com>
2024-02-21 22:27:19 +0000

    KIOOPP

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * init_dog - Using the variables of dog.h
+ * @d: struct with dog variables
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Dog owner
+ * Return: 0
  */
-int main(void)
-{
-    struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
+
