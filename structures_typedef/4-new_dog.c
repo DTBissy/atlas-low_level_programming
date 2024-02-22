6db9442d935481d1b11fed8f7821a93bcd54426c
DTBIssy <isaactheedwards@gmail.com>
2024-02-22 01:08:55 +0000

    LO

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
-#include <string.h>
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+#include <string.h>
 
 /**
- * new_dog - Creates a new dog 
- * @name: Name of doggy
- * @age: Age of doggy
- * @owner: Owner of doggy
+ * new_dog - New dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Dog owner
  * Return: 0
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	/** Allocates memory for the new dog */
-	dog_t *new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
-		return NULL;
-	}
-	/** Allocates memory for the new name + 1 for '\0' */
-	new_dog->name = malloc(strlen(name) + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog); /**Frees the memory for dog */
-		return NULL;
-	}
-	strcpy(new_dog->name, name);
+dog_t *new_dog(char *name, float age, char *owner) {
+    dog_t *new_dog;
 
-	new_dog->age = age;
+    /** Allocate memory for the new dog struct*/
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL) {
+        return NULL;
+    }
 
-	new_dog->owner = malloc(strlen(owner) + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name); /**Frees the memory allocated for the name*/
-		free(new_dog);/**Free the memory for the dog pointer */
-		return NULL;
-	}
-	strcpy(new_dog->owner, owner);
+    /** Allocate memory for the name and copy it*/
+    new_dog->name = malloc(strlen(name) +  1);
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->name, name);
 
-	return new_dog;
+    /** Set the age*/
+    new_dog->age = age;
+
+    /** Allocate memory for the owner and copy it*/
+    new_dog->owner = malloc(strlen(owner) +  1);
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->owner, owner);
+
+    return new_dog;
 }
