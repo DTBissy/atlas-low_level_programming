fc763fcc46267bb35bbb1a91c7139d7799f5a971
DTBissy <isaactheedwards@gmail.com>
2024-02-23 21:29:23 +0000

    CALCULATOR

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -21,12 +21,22 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
 	printf("Sum: %d\n", a / b);
 	exit(0);
 }
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
 	printf("Sum: %d\n", a % b);
 	exit(0);
 }
