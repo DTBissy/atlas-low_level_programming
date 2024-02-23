8f230075121749c0eb420659b57c0e998cd5a873
DTBissy <isaactheedwards@gmail.com>
2024-02-23 22:21:55 +0000

    Hmph

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("ERROR\n");
-		return (98);
+		exit(98);
 	}
 	
 	if (argv[2][1])
 	{
 		printf("ERROR\n");
-		return (99);
+		exit(99);
 	}
 
 	opera = get_op_func(argv[2]);
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
 	if (opera == NULL)
 	{
 		printf("ERROR\n");
-		return (99);
+		exit(99);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	printf("%d\n", opera(a, b));
-	return (0);
+	exit(0);
 }
