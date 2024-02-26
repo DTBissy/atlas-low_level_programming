834b4e1c480bd5d000262becb6cfa2c1e7c30899
DTBissy <isaactheedwards@gmail.com>
2024-02-26 01:39:26 +0000

    Easy switch but char* is nessacary here

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
+#include "variadic_functions.h"i
+
+/**
+ * print_strings - Prints strings
+ * @separator: The comma with the space
+ * @n: The string to print
+ * Return: 0
+ */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
