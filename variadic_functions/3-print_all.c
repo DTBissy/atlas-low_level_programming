5d9d56ae0e3a6f94d25a740c79415d835ce2baa2
DTBissy <isaactheedwards@gmail.com>
2024-02-26 04:45:26 +0000

    LOl

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,40 +9,41 @@ void print_all(const char * const format, ...)
 	double f;
 	char c;
 	const char *format_copy = format;
-	
+
 	va_list gah;
 	va_start(gah, format);
 
-	while (*format_copy != '\0')
+	while (*format_copy)
 	{
-		if (*format_copy == 'c')
+		switch (*format_copy)
 		{
-			c = va_arg(gah, int);
-			printf("%c", c);
+			case 'c':
+				c = va_arg(gah, int);
+				printf("%c, ", c);
+				break;
+			case 'i':
+				i = va_arg(gah, int);
+				printf("%d, ", i);
+				break;
+			case 'f':
+				f = va_arg(gah, double);
+				printf("%f, ", f);
+				break;
+			case 's':
+				s = va_arg(gah, char*);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				break;
+			default:
+				break;
 		}
-		if (*format_copy == 'i')
-		{
-			i = va_arg(gah, int);
-			printf("%d", i);
-		}
-		if (*format_copy == 'f')
-		{
-			f = va_arg(gah, double);
-			printf("%f", f);
-		}
-		if (*format_copy == 's')
-		{
-			s = va_arg(gah, char*);
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-			printf("%s", s);
-			}
-		}
-	format_copy++;
+		format_copy++;
 	}
 	va_end(gah);
 	printf("\n");
