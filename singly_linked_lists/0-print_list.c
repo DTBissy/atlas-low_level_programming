851b6bdc4677afd912a53fcdcbd095b512af1d8e
DTBissy <isaactheedwards@gmail.com>
2024-03-06 22:15:27 +0000

    "another try"

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @list_t *h: Head node
+ * Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
