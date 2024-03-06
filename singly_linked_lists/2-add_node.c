323b7e0f537dbfa121affbda03ca148c474be833
DTBissy <isaactheedwards@gmail.com>
2024-03-06 23:11:31 +0000

    No StRlEn UhHhH

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,6 +9,8 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	int len = 0;
+
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -21,7 +23,11 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
