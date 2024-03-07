cf90510199693b996ded4896674268cf7d6bde0b
DTBissy <isaactheedwards@gmail.com>
2024-03-07 22:15:39 +0000

    more

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,33 +7,34 @@
  * Return: 0
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index) 
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current = *head;
-    listint_t *previous = NULL;
-    unsigned int currentIndex = 0;
+	listint_t *current = *head;
+	listint_t *previous = NULL;
+	unsigned int currentIndex = 0;
 
-    if (head == NULL || *head == NULL) {
-        return -1;
-    }
+	if (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 
-    if (index == 0) {
-        *head = current->next;
-        free(current);
-        return 1;
-    }
-
-    while (current != NULL && currentIndex != index) {
-        previous = current;
-        current = current->next;
-        currentIndex++;
-    }
-
-    if (current == NULL) {
-        return -1; 
-    }
-    
-    previous->next = current->next;
-    free(current);
-    return 1;
+	while (current != NULL && currentIndex != index)
+	{
+		previous = current;
+		current = current->next;
+		currentIndex++;
+	}
+	if (current == NULL)
+	{
+		return (-1);
+	}
+	previous->next = current->next;
+	free(current);
+	return (1);
 }
