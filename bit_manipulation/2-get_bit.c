123219f61be9b13cd0c021e1217651f944269d3b
DTBissy <isaactheedwards@gmail.com>
2024-03-09 02:58:04 +0000

    Hmph

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the bit at a givien index
+ * @n:Number to be checked
+ * @index: Spot in binary
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	int bit;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	mask = 1UL << index;
+	bit = (n & mask) ? 1 : 0;
+	return bit;
+}
+
+
+
