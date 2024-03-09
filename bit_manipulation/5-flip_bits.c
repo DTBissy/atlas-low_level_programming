bfc71d4acf8cf3b22cff6db79f9593cdcf57e60d
DTBissy <isaactheedwards@gmail.com>
2024-03-09 04:10:14 +0000

    Ok doing the XOR in the result is cool

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * flip_bits - eturns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - flips bit from one number to another.
  * @n: first number
  * @m: number to flip to
  * Return: 0
@@ -15,5 +15,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		count += result & 1;
 		result >>= 1;
 	}
-	return count;
+	return (count);
 }
