c4051826d26d0aa02c6f4cfd2c76d0b074b0c381
DTBissy <isaactheedwards@gmail.com>
2024-03-13 22:20:05 +0000

    Betty geekin

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
- * print_dlistint - Prints all elements of struct
- * @h: head node
- * Return: 0
+* print_dlistint - Prints all elements of struct
+* @h: head node
+* Return: 0
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    
-    while (h != NULL)
-    {
-         printf("%d\n", h->n);
-          h = h->next;
-          count++;
-          }
-          return (count);
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+		}
+		return (count);
 }
