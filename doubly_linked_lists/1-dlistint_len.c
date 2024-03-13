f7ad6aaa3c13cda358707c7b3e3a340ee3cd14b8
DTBissy <isaactheedwards@gmail.com>
2024-03-13 23:22:01 +0000

    Carnival is amazing

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* print_dlistint - Prints all elements of struct
+* @h: head node
+* Return: 0
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
