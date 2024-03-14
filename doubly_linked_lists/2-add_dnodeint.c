979c51d77f56f9749c279f1f9895f8d5f32710ef
DTBissy <isaactheedwards@gmail.com>
2024-03-14 00:04:47 +0000

    Had to delete the free and ad an if for if the *head was not NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->n = n;
 	newnode->prev = NULL;
    newnode->next = *head;
-   if (*head = NULL)
+   if (*head != NULL)
    {
       (*head)->prev = newnode;
    }
