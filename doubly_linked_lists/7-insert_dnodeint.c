0ad827fbf654b06d95572d6318f0cf09f7d3cd19
DTBissy <isaactheedwards@gmail.com>
2024-03-14 02:40:02 +0000

    Dont car about 40 more lines its stupid anway

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,25 +19,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	newnode->n = n;
 	newnode->next = NULL;
-
+	newnode->prev = NULL;
 	if (idx == 0)
 	{
+		if (*h != NULL)
+		{
+			(*h)->prev = newnode;
+		}
 		newnode->next = *h;
 		*h = newnode;
-		return (newnode);
 	}
-	while (current != NULL && i < idx - 1)
+	else
 	{
-		current = current->next;
-		i++;
-	}
+		while (current != NULL && i < idx - 1)
+		{
+			current = current->next;
+			i++;
+		}
 
-	if (current == NULL)
-	{
-		free(newnode);
-		return (NULL);
+		if (current == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+		newnode->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = newnode;
+		}
+		current->next = newnode;
+		newnode->prev = current;
 	}
-	newnode->next = current->next;
-	current->next = newnode;
 	return (newnode);
 }
