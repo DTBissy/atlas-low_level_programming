445ef94191ab349ef506f421bf2711da81af71bf
DTBissy <isaactheedwards@gmail.com>
2024-03-15 23:23:46 +0000

    Simple enough

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,4 +10,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
   unsigned long int index = 0;
   hash_node_t* node = NULL, *add_node = NULL, *tmp = NULL;
-}
+
+  if (!key || !ht || !ht->array)
+  {
+    return (0);
+  }
+  index = key_index((unsigned char*)key, ht->size);
+  node = ht->array[index];
+
+  if (!node)
+  {
+    node = malloc(sizeof(node));
+    if (!node)
+    {
+      return (0);
+    }
+  }
+  node->key = strdup(key;
+  node->value = strdup(value);
+  node->next = NULL;
+  ht->array = node;
+  )
