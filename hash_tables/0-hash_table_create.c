16b471bf6dba7c43d02a1fb01098590f15b898a8
DTBissy <isaactheedwards@gmail.com>
2024-03-15 19:39:24 +0000

    OK

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - Creates a hash table
- * @size: Size of hash array
- * Return: 0
+* hash_table_create - Creates a hash table
+* @size: Size of hash array
+* Return: 0
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t * hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-    {
-        return (NULL);
-    }
-    size = size;
-    return (hash_table);
+hash_table_t *hash_table = malloc(sizeof(hash_table_t) * 1);
+if (hash_table == NULL)
+{
+return (NULL);
+}
+size = size;
+return (hash_table);
 }
