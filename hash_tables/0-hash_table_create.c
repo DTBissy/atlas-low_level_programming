0d719fbd819d50651089b1c369241d00682011c1
DTBissy <isaactheedwards@gmail.com>
2024-03-15 19:45:45 +0000

    Lets try

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,11 +6,11 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table = malloc(sizeof(hash_table_t) * 2);
+hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
 {
 return (NULL);
 }
-size = size;
+size <<= size;
 return (hash_table);
 }
