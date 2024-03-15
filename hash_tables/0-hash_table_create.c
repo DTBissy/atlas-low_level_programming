4a8ae7d1fee1d1c66965861ad8a8e8edb8442d88
DTBissy <isaactheedwards@gmail.com>
2024-03-15 20:20:36 +0000

    Betty COmpliant

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,22 +1,23 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - Creates a hash table
- * @size: Size of hash array
- * Return: 0
- */
+* hash_table_create - Creates a hash table
+* @size: Size of hash array
+* Return: 0
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = (hash_table_t*)malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return (NULL);
-    }
-    table->size = size;
-    table->array = (hash_node_t**)calloc(size, sizeof(hash_node_t*));
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
-    return (table);
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }
