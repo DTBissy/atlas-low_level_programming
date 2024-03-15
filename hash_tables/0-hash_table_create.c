d9cfae28a62d668790776526157236e36ee45ad8
DTBissy <isaactheedwards@gmail.com>
2024-03-15 19:52:46 +0000

    Moo

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,10 +7,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-if (hash_table == NULL)
-{
-return (NULL);
-}
-size <<= size;
+size = size;
 return (hash_table);
 }
