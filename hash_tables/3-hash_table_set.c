65bbf48de4d4f387e65bae3af395b474112c6f29
DTBissy <isaactheedwards@gmail.com>
2024-03-16 17:31:04 +0000

    Add node

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,41 +8,43 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  unsigned long int index = 0;
-  hash_node_t* node = NULL, *add_node = NULL, *tmp = NULL;
+	unsigned long int index = 0;
+	hash_node_t *node = NULL, *add_node = NULL, *tmp = NULL;
 
-  if (!key || !ht || !ht->array)
-  {
-    return (0);
-  }
-  index = key_index((unsigned char*)key, ht->size);
-  node = ht->array[index];
+	if (!key || !ht || !ht->array)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-  if (!node)
-  {
-    node = malloc(sizeof(node));
-    if (!node)
-    {
-      return (0);
-    }
-  }
-  node->key = strdup(key;
-  node->value = strdup(value);
-  node->next = NULL;
-  ht->array = node;
+	if (!node)
+	{
+		node = malloc(sizeof(node));
+		if (!node)
+			return (0);
+	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	ht->array[index] = node;
 
-  for (tmp = node; tmp; tmp = tmp->next)
-  {
-    if (strcmp(tmp->next, key) == 0))
-    {
-      free(tmp->value), tmp->value = strdup(value);
-      return (1);
-    }
-    add_node = malloc(sizeof(hash_node_t));
-    if (!add_node)
-    {
-      return (0);
-    }
-    add_node
-  }
+	for (tmp = node; tmp; tmp = tmp->next)
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value), tmp->value = strdup(value);
+			return (1);
+		}
+	add_node = malloc(sizeof(hash_node_t));
+
+	if (!add_node)
+	{
+		return (0);
+	}
+	add_node = malloc(sizeof(hash_node_t));
+	if (!add_node)
+		return (0);
+	add_node->key = strdup(key);
+	add_node->next = node;
+	ht->array[index] = add_node;
+
+	return (1);
 }
