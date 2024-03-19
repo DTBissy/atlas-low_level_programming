4a6767563e4ead5d987b348c7c3cafbe1229dacb
DTBissy <isaactheedwards@gmail.com>
2024-03-19 00:21:44 +0000

    Updating key index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 unsigned long int index = 0;
 
-index = djb2(key);
+index = hash_djb2(key);
 index %= size;
 return (index);
 }
