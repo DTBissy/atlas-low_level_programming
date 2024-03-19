df25543785e784640fe54021826cd021161cd4a8
DTBissy <isaactheedwards@gmail.com>
2024-03-19 01:21:33 +0000

    Were done

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,19 +1,27 @@
 #include "hash_tables.h"
 
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index = 0;
-  hash_table_t *current = ht;
+unsigned long int index = 0;
 
-  while (current != NULL)
-  {
-    if (index == key)
-    {
-      return (key);
-    }
-    index++;
-    current = current->next;
-  }
-  return (0);
+hash_node_t *node = NULL;
+hash_node_t *tmp = NULL;
+
+if (!ht || !ht->array || !key)
+{
+	return (NULL);
+}
+
+index = key_index((unsigned char *)key, ht->size);
+
+node = ht->array[index];
+if (!node)
+	return (NULL);
+if (strcmp(node->key, key) == 0)
+	return (node->value);
+
+for (tmp = node; tmp; tmp = tmp->next)
+	if (strcmp(tmp->key, key) == 0)
+	return (tmp->value);
+return (NULL);
 }
