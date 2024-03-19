7a91409c6849002a7c21333cd3bc4e710d805010
DTBissy <isaactheedwards@gmail.com>
2024-03-19 00:40:20 +0000

    Soldados

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,1 +1,19 @@
-#include "lists_h"
+#include "hash_tables.h"
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  unsigned long int index = 0;
+  hash_table_t *current = ht;
+
+  while (current != NULL)
+  {
+    if (index == key)
+    {
+      return (key);
+    }
+    index++;
+    current = current->next;
+  }
+  return (0);
+}
