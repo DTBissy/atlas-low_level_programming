98f2adbeeb0b606a217139b59c8940a93948213d
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-19 20:48:48 -0500

    Or is this something i Love

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* append_text_to_file - Appends text to a file
+* @filename: The file created
+* @text_content: The content
+* Return: A succesful append
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, len = 0, bw;
+
+if (filename == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_RDWR | O_APPEND);
+if (fd < 0)
+return (-1);
+if (text_content == NULL)
+{
+close(fd);
+return (-1);
+}
+
+while (*(text_content + len))
+len++;
+
+bw = write(fd, text_content, len);
+if (bw < 0)
+{
+close(fd);
+return (-1);
+}
+return (1);
+}
