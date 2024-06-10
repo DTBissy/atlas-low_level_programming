e6c5f3375954710ef2840ebc4419d228be7ad16e
Dtbissy <isaactheedwards@gmail.com>
2024-06-10 14:43:15 -0500

    as

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,16 +3,16 @@
 #include <ctype.h>
 
 /**
-* _isdigit - Checks for a digit 0 - 9
-* @c: function parameter
-* Return: 0
-*/
+ * _isdigit - Checks for a digit 0 - 9
+ * @c: function parameter
+ * Return: 0
+ */
 
 int _isdigit(int c)
 {
-/** these numbers are the ASCII numbers for 0 and 9 */
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+  /** these numbers are the ASCII numbers for 0 and 9 */
+  if (c >= 48 && c <= 57)
+    return (1);
+  else
+    return (0);
 }
